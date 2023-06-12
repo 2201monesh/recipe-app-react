6f3cf6c48816b29702684d50a4333c0e451dc5a2
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-06-12 15:01:53 +0530

    Adding Router functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Category from "./components/Category";
 import Pages from "./pages/Pages";
 
 function App() {
   return (
     <div className="App">
-      hello
+      <Category />
       <Pages />
     </div>
   );
