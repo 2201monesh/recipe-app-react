745a9db463d699c33ecc3604e6451497ea779516
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-06-13 17:06:13 +0530

    Recipe Page added

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { styled } from 'styled-components';
+import styled from 'styled-components';
 
 function Searched() {
 
